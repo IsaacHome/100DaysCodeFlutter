aee0a3f851d98dc7fd387eeae59f2655f524643a
isaachome <g.isaacvhm@gmail.com>
2019-08-24 20:38:54 +0630

    Day 2 flutter  100DaysCode

---
+++ bottomnavigationbar/lib/pages/airplay_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AirplayScreen extends StatelessWidget{
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         title: Text("Airplay Page"),
+       ),
+      body: Center(
+        child: Text("AirPlay Screen"),
+      ),
+    );
+  }
+}
